924a0cc612ec969cabcc4ae4f9531fe25c86da52
刘勇七 <1508414512@qq.com>
2018-09-27 21:31:12 +0800

    haha

--- kick.py
+++
@@ -1,8 +0,0 @@
-# _*_ encoding:utf-8 _*_
-__author__ = 'LYQ'
-__data__ = '2018/8/7 9:47'
-import requests
-from bs4 import BeautifulSoup
-# proxies = {'https': '8.8.8.8:1080'}
-response=requests.get('https://www.kickstarter.com/projects/edgeofbelgravia/shiroi-hana-chef-knife-collection-japanese-steel?ref=discovery')
-print(response.text)
